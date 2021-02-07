6cd752c3b4cac55d31fccbea82a86ff2694c95a9
Khaled Issa <khaled.ielmt@hotmail.com>
2021-02-07 19:02:27 +0200

    added elasticsearch endpoint

--- instabug_chat_system/app/models/message.rb
+++ instabug_chat_system/app/models/message.rb
@@ -1,3 +1,40 @@
 class Message < ApplicationRecord
   belongs_to :chat
+
+
+  validates_presence_of :app_token, :chat_number, :message
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  index_name    'instabugmessage'
+  document_type 'message'
+
+  es_index_settings = {
+    'analysis': {
+      'filter': {
+        'trigrams_filter': {
+          'type':'ngram',
+          'min_gram': 3,
+          'max_gram': 3
+        }
+      },
+      'analyzer': {
+        'trigrams': {
+          'type': 'custom',
+          'tokenizer': 'standard',
+          'filter': [
+            'lowercase',
+            'trigrams_filter'
+          ]
+        }
+      }
+    }
+  }
+
+  settings es_index_settings do
+    mapping do
+      indexes :message, type: 'string', analyzer: 'trigrams'
+    end
+  end
 end
